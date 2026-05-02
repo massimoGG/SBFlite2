a8392e8e6711af6a62e26ee188483d8516935fc2
massimoGG <11910454+massimoGG@users.noreply.github.com>
2026-05-02 11:29:19 +0200

    [V2.0.0] Refactor project

---
+++ src/inverter.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file inverter.cpp
+ * @brief implemnetation of an SMA inverter
+ */
+
+#include "inverter.hpp"
+
+/**
+ * @brief forms a printable inverter
+ * @param inv
+ * @returns string representation of given inverter
+ */
+std::string to_string(const SmaInverter_t &inv)
+{
+    using namespace std;
+
+    string ret{};
+
+    ret += "\n\n\n\033[1m---------------------------\nINVERTER - " + string(inv.Name) + "\n" + string(inv.Ip) + "%s\n---------------------------\033[0m\n";
+
+    ret += "Total yield: " + to_string(inv.TotalYield) + "Wh\n";
+
+    ret += "Day yield: " + to_string(inv.DayYield) + "Wh\n";
+
+    ret += "Inverter\n\tTemperature: " + to_string(inv.Temperature) + "C\tHeatsink: " + to_string(inv.HeatsinkTemperature) + "C\n";
+
+    ret += "DC 1\n\tVolt: " + to_string(inv.Udc1) + "V\n\tAmp: " + to_string(inv.Idc1) + "A\n\tWatt: " + to_string(inv.Pdc1) + "W\n";
+    ret += "DC 2\n\tVolt: " + to_string(inv.Udc2) + "V\n\tAmp: " + to_string(inv.Idc2) + "A\n\tWatt: " + to_string(inv.Pdc2) + "W\n";
+    ret += "AC  \n\tVolt: " + to_string(inv.Uac1) + "V\n\tAmp: " + to_string(inv.Iac1) + "A\n\tWatt: " + to_string(inv.Pac1) + "W\n";
+
+    ret += "\tGridFreq: " + to_string(inv.GridFreq) + "\n\tReactiveP: " + to_string(inv.ReactivePower) + " VAr\n\tApparentP: " + to_string(inv.ApparentPower) + "\n";
+
+    return ret;
+}
---
+++ src/modbus.hpp
@@ -0,0 +1,54 @@
+#ifndef __MODBUS_HPP__
+#define __MODBUS_HPP__
+
+#include <cstdint>
+
+#define RETRIES 3
+#define DEBUG 0
+
+#define MODBUS_READ_HOLDING_REGISTERS 0x03
+#define MODBUS_READ_INPUT_REGISTERS 0x04
+
+#define MODBUS_MAX_FRAME_LENGTH 260
+#define MODBUS_TCP_REQ_LENGTH 12
+#define MODBUS_DATA_OFFSET 9
+
+// How many seconds to wait before attemping again
+#define MODBUS_SMA_WAIT 1
+
+enum
+{
+    MODBUS_EXCEPTION_ILLEGAL_FUNCTION = 0x01,
+    MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS = 0x02,
+    MODBUS_EXCEPTION_ILLEGAL_DATA_VALUE = 0x03,
+};
+
+enum
+{
+    eModbus_port = 502,
+};
+
+typedef struct
+{
+    int s;
+
+    unsigned char slave;
+    unsigned short transaction_id;
+
+    char *ip;
+    unsigned short port;
+} modbus_t;
+
+typedef uint8_t *modbus_regs;
+
+/**
+ * Function predefinitions
+ */
+modbus_t *modbus_connect_tcp(const char *ip, unsigned short port);
+int modbus_build_request_header(modbus_t *mb, unsigned char function, unsigned short addr, unsigned short qoc, uint8_t *pkg);
+unsigned long getValue(modbus_regs regs, unsigned short begin, unsigned short indexAddress);
+modbus_regs modbus_read_registers(modbus_t *mb, int addr, int qoc);
+void modbus_free_registers(modbus_regs regs);
+void modbus_close(modbus_t *t);
+
+#endif
